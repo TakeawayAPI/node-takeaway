b3fae2ed725b2855a139279c96c9c09720b2ed4e
Daniel Huisman <daniel@huisman.me>
2018-05-07 00:54:59 +0200

    Move example code to example folder

---
+++ example/index.js
@@ -0,0 +1,36 @@
+import {inspect} from 'util';
+
+import {TakeawayConfig, TakeawayClient} from '../src';
+
+(async () => {
+    try {
+        const config = new TakeawayConfig();
+        const client = new TakeawayClient(config);
+
+        // const data = await client.getRestaurants({
+        //     postalCode: '7523',
+        //     country: '1',
+        //     latitude: '52.0000000',
+        //     longitude: '6.0000000',
+        //     language: 'nl'
+        // });
+
+        // const data = await client.getMenuCard({
+        //     restaurantId: '1N01N',
+        //     postalCode: '7523',
+        //     latitude: '52.0000000',
+        //     longitude: '6.0000000'
+        // });
+
+        // const data = await client.getConfig();
+        const data = await client.getReviews({
+            restaurantId: '1N01N'
+        }, {
+            debug: true
+        });
+
+        console.log(inspect(data, false, null));
+    } catch (err) {
+        console.error(err);
+    }
+})();
