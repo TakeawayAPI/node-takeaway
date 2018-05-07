40c152404c355f788df1f7591c481d9cc9e4e61b
Daniel Huisman <daniel@huisman.me>
2018-05-07 22:28:28 +0200

    Fix exports

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,8 @@
 export {default as Address} from './Address';
 export {default as Category} from './Category';
+export {default as Choice} from './Choice';
 export {default as Country} from './Country';
+export {default as Option} from './Option';
+export {default as Product} from './Product';
 export {default as Restaurant} from './Restaurant';
+export {default as User} from './User';
