1a6a1f7990e10618ea5a18183b65f99b98bbbcac
Daniel Huisman <daniel@huisman.me>
2019-08-13 14:30:08 +0200

    Add multiple country example

---
+++ example/countries.js
@@ -0,0 +1,53 @@
+import {inspect} from 'util';
+
+import {Takeaway} from '../src';
+
+const DEBUG = false;
+
+const print = (obj) => {
+    if (DEBUG) {
+        console.log(inspect(obj, false, null));
+    }
+};
+
+const inputs = [{
+    language: 'nl',
+    url: 'https://nl.citymeal.com/android/android.php',
+    postalCode: '7522'
+}, {
+    language: 'de',
+    url: 'https://de.citymeal.com/android/android.php',
+    postalCode: '44369'
+}];
+
+(async () => {
+    try {
+        // Choose input
+        const input = inputs[0];
+
+        // Initialize API
+        const takeaway = new Takeaway({
+            language: input.language,
+            url: input.url
+        });
+
+        // Get country
+        const country = await takeaway.getCountryById(input.language.toUpperCase());
+        print(country.data);
+
+        // Get restaurants for the postal code
+        const restaurants = await country.getRestaurants(input.postalCode, '', '');
+
+        // Loop over all restaurants
+        for (const restaurant of restaurants) {
+            print(restaurant);
+
+            const name = restaurant.data.name;
+            const address = `${restaurant.address.data.street}, ${restaurant.address.data.city}`;
+
+            console.log(`${name} - ${address} - ${restaurant.data.open ? 'OPEN' : 'CLOSED'}`);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+})();
